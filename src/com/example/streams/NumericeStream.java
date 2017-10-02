fdfaabfa5868407eea5d8e67c102dac9f68516b4
Amit Patil <iamitpatil1993@gmail.com>
2017-10-02 19:12:17 +0530

    test

--- src/com/example/streams/NumericeStream.java
+++ src/com/example/streams/NumericeStream.java
@@ -8,11 +8,11 @@ import java.nio.file.Paths;
 public class NumericeStream {
 
 	public static void main(String[] args) {
-		
+
 		try {
-			
+
 			Files.lines(Paths.get("file.txt"), Charset.defaultCharset()).forEach(System.out::println);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
